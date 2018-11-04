4e85ac0cecb5bf7586bb58971f058bab77ea0f63
mhudnell-desk <maxhudnell@gmail.com>
2018-11-03 23:48:38 -0400

    add dataset and display arguments to drawFrameRecs(), add methods to Rect class:   get_intersection()   get_IoU()   get_DE() fix get_IoU non-intersection bug, add calc_metrics (currently unused) to compute both IoU and DE in future

--- vis_tool.py
+++ vis_tool.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import data_extract_1obj
 
-def drawFrameRects(sample_set, frame, objId, bb, isGen, folder_dir):
+def drawFrameRects(sample_set, frame, objId, bb, isGen, folder_dir, dataset='kitti_tracking', display=False):
     '''
     Draws the provided bounding boxes onto the specified frame and outputs the drawn image to 'folder_dir'.
 
@@ -14,14 +14,33 @@ def drawFrameRects(sample_set, frame, objId, bb, isGen, folder_dir):
         isGen (boolean): Indicates whether the concluding bounding box in 'bb' was generated, or if it is the target.
         folder_dir: The folder to output the drawn images to.
     '''
-    img_file = 'F:\\Car data\\kitti\\data_tracking\\training\\image_02\\' + sample_set + '\\' + frame + '.png'
+    print(sample_set)
+    if dataset == 'kitti_raw':
+        img_file = sample_set + '\\image_02\\data\\' + frame + '.png'
+    elif dataset == 'kitti_raw_tracklets':
+        img_file = sample_set + '\\image_02\\data\\' + frame + '.png'
+    else:
+        img_file = 'F:\\Car data\\kitti\\data_tracking\\training\\image_02\\' + sample_set + '\\' + frame + '.png'
 
+    print(img_file)
     # Load img to draw on.
     img = cv2.imread(img_file)
 
     proposal = data_extract_1obj.transform(bb[-2], bb[-1])
     bb[-1] = proposal
-    unnormal = data_extract_1obj.unnormalize_sample(bb, sample_set)
+
+    if dataset in ['kitti_raw', 'kitti_raw_tracklets']:
+        unnormal = np.empty(bb.shape)
+        unnormal[:, 0] = bb[:, 0] * 1242
+        unnormal[:, 1] = bb[:, 1] * 375
+        unnormal[:, 2] = bb[:, 2] * 1242
+        unnormal[:, 3] = bb[:, 3] * 375
+        # print("unnormal:", unnormal)
+        for i, bb in enumerate(unnormal):
+            unnormal[i] = data_extract_1obj.center_to_topleft_bb(bb)
+        # print("top-left pix:", unnormal)
+    else:
+        unnormal = data_extract_1obj.unnormalize_sample(bb, sample_set)
     # topleft = data_extract_1obj.center_to_topleft_bb(unnormal)
 
     # Convert bb (LTWH values) to ints.
@@ -45,12 +64,20 @@ def drawFrameRects(sample_set, frame, objId, bb, isGen, folder_dir):
 
     # Write image to file.
     suffix = "generated" if isGen else "target"
-    cv2.imwrite(folder_dir + sample_set + '_' + frame + '_' + objId + '_' + suffix + '.png', img)
+
+    if display:
+        cv2.imshow('ImageWindow', img)
+        cv2.waitKey()
+    else:
+        cv2.imwrite(folder_dir + sample_set + '_' + frame + '_' + objId + '_' + suffix + '.png', img)
 
     return
 
 class Rect:
     def __init__(self, l, t, r, b):
+        assert (l < r), "l should be less than r"
+        assert (t < b), "t should be less than b"
+
         self.l = l
         self.t = t
         self.r = r
@@ -74,6 +101,34 @@ class Rect:
         b = t + h
         return cls(l, t, r, b)
 
+    @classmethod
+    def get_intersection(cls, rect1, rect2):
+        l = max(rect1.l, rect2.l)
+        t = max(rect1.t, rect2.t)
+        r = min(rect1.r, rect2.r)
+        b = min(rect1.b, rect2.b)
+
+        if l < r and t < b:
+            return cls(l, t, r, b)
+        else:
+            return False
+
+    @staticmethod
+    def get_IoU(rect1, rect2):
+        intersect = Rect.get_intersection(target, generated)
+        if intersect:
+            iou = intersect.area / (target.area + generated.area - intersect.area)
+            assert (iou > 0), "Non-positive IoU!"
+            return iou
+        return 0
+    
+    @staticmethod
+    def get_DE(rect1, rect2):
+        c1 = np.array([(rect1.l + rect1.r)/2, (rect1.t + rect1.b)/2])
+        c2 = np.array([(rect2.l + rect2.r)/2, (rect2.t + rect2.b)/2])
+        return np.linalg.norm(c1 - c2)
+
+
 def get_IoU(anchor, target_transform, generated_transform, sample_set=None):
     t_bb = data_extract_1obj.transform(anchor, target_transform)
     g_bb = data_extract_1obj.transform(anchor, generated_transform)
@@ -82,7 +137,54 @@ def get_IoU(anchor, target_transform, generated_transform, sample_set=None):
 
     target = Rect.make_cXcYWH(t_bb[0], t_bb[1], t_bb[2], t_bb[3])
     generated = Rect.make_cXcYWH(g_bb[0], g_bb[1], g_bb[2], g_bb[3])
-    intersect = Rect(max(target.l, generated.l), max(target.t, generated.t),
-                     min(target.r, generated.r), min(target.b, generated.b))
+    intersect = Rect.get_intersection(target, generated)
 
-    return intersect.area / (target.area + generated.area - intersect.area)
+    if intersect:
+        iou = intersect.area / (target.area + generated.area - intersect.area)
+        assert (iou > 0), "Non-positive IoU!"
+        return iou
+
+    return 0
+
+def calc_metrics(anchor, target_transform, generated_transform, sample_set=None):
+    """ Calculates displacement error and IoU metrics """
+    t_bb = data_extract_1obj.transform(anchor, target_transform)
+    g_bb = data_extract_1obj.transform(anchor, generated_transform)
+    t_bb = data_extract_1obj.unnormalize_bb(t_bb, sample_set=sample_set)
+    g_bb = data_extract_1obj.unnormalize_bb(g_bb, sample_set=sample_set)
+
+    target = Rect.make_cXcYWH(t_bb[0], t_bb[1], t_bb[2], t_bb[3])
+    generated = Rect.make_cXcYWH(g_bb[0], g_bb[1], g_bb[2], g_bb[3])
+
+    iou = Rect.get_IoU(target, generated)
+    de = Rect.get_DE(target, generated)
+
+    return [iou, de]
+
+
+if __name__ == '__main__':
+
+    # samples, samples_info = data_extract_1obj.get_kitti_data(sets=None)
+    # samples, samples_info = data_extract_1obj.get_kitti_raw_data()
+    samples, samples_info = data_extract_1obj.get_kitti_raw_tracklets()
+
+    # print(samples.shape)
+    # print(len(samples_info))
+
+    # x = samples[0]
+    # x_info = samples_info[0]
+    # print(x)
+    # print(x_info)
+
+    # drawFrameRects(x_info[0], x_info[1], x_info[2], x, False, None, dataset='kitti_raw', display=True)
+    # drawFrameRects(x_info[0], x_info[1], x_info[2], x, False, None, dataset='kitti_raw_tracklets', display=True)
+
+    # View multiple
+    # for i in range(5):
+    #     x = samples[i]
+    #     x_info = samples_info[i]
+
+    #     print(x)
+    #     print(x_info)
+
+    #     drawFrameRects(x_info[0], x_info[1], x_info[2], x, False, None, dataset='kitti_raw_tracklets', display=True)
